0de56d384e9313176047ae10fb80b25a64e4823f
reddynie11 <reddy.nie11@gmail.com>
2019-10-03 21:11:08 +0530

    homepage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import HomePage from './homepage.component'
 
 function App() {
   return (
     <div className="App">
-  
+      <HomePage />
     </div>
   );
 }
