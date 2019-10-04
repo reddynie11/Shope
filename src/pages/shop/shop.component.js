f45147974eef77a5d4a609d3adf065cf4273f609
reddynie11 <reddy.nie11@gmail.com>
2019-10-04 14:57:06 +0530

    shop page

---
+++ src/pages/shop/shop.component.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import SHOP_DATA from './shop.data';
+
+import CollectionPreview from '../../components/preview-collection/collection-preview.component';
+
+class ShopPage extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state={
+            collections: SHOP_DATA
+        }
+    }
+    
+    render(){
+        return(
+            <div className='shop-page'>
+                {this.state.collections.map((collection)=>(
+                    <CollectionPreview key={collection.id} title={collection.title} items={collection.items} />
+                ))}
+            </div>
+        )
+    }
+}
+export default ShopPage;
