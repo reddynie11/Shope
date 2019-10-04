9a93dd3e41d7e18e403d2cc2bc5260eeba50d895
reddynie11 <reddy.nie11@gmail.com>
2019-10-04 15:57:29 +0530

    filter in collection preview

--- src/components/preview-collection/collection-preview.component.js
+++ src/components/preview-collection/collection-preview.component.js
@@ -7,10 +7,9 @@ const CollectionPreview = ({title, items})=>{
         <div className='collection'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <div className='preview' >
-                {
-                    items.map(item => (
-                        <div key={item.id}>{item.name}</div>
-                    ))
+                {items
+                    .filter((item,i)=> i < 4)
+                    .map(item => ( <div key={item.id}>{item.name}</div>))
                 }
             </div>
         </div>
