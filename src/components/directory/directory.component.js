fddbb722c2cdd2508bb27e8f53c908ebb0c5abf6
reddynie11 <reddy.nie11@gmail.com>
2019-10-06 15:31:14 +0530

    HomePage UI updated

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -8,38 +8,40 @@ class Directory extends React.Component{
         super();
         this.state={
             sections : [
-                {
+              {
+                title: 'mens',
+                imageUrl: 'https://i.ibb.co/BKRqYWv/men.jpg',
+                size: 'large',
+                id: 5,
+                linkUrl: ''
+              },
+                  {
                     title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    imageUrl: 'https://i.ibb.co/XWmKDbk/hats.jpg',
                     id: 1,
                     linkUrl: 'hats'
                   },
+                 
                   {
                     title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    imageUrl: 'https://i.ibb.co/MPmRV9r/jacket.jpg',
                     id: 2,
                     linkUrl: ''
                   },
                   {
                     title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    imageUrl: 'https://i.ibb.co/q1FWSqr/sneakers.jpg',
                     id: 3,
                     linkUrl: ''
-                  },
-                  {
+                  }, {
                     title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    imageUrl: 'https://i.ibb.co/wJC5GDX/women.jpg',
                     size: 'large',
                     id: 4,
                     linkUrl: ''
-                  },
-                  {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                    size: 'large',
-                    id: 5,
-                    linkUrl: ''
                   }
+                 
+                  
             ]
         }
     }
